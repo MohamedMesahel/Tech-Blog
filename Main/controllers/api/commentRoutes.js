e7aa06365156590b5ffb0029154c355d470618bf
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-03 15:50:04 -0700

    adding the comment router to index.js

--- Main/controllers/api/commentRoutes.js
+++ Main/controllers/api/commentRoutes.js
@@ -17,7 +17,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const commentData = await Project.destroy({
+    const commentData = await Comment.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
