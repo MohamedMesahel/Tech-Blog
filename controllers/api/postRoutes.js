670323fccd14c4d7ac80673a54859067b0ce6652
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-05 13:11:40 -0700

    building postsRoutes using async methoad

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,56 +2,121 @@
 
 const router = require("express").Router();
 const { Post, Comment, User } = require("../../models");
-const withAuth = require("../../Main/utils/auth");
+const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, (req, res) => {
-    const body = req.body;
-    console.log(req.session.userId);
-    Post.create({ ...body, userId: req.session.userId })
-        .then(newPost => {
-            res.json(newPost);
-        })
-        .catch(err => {
-            res.status(500).json(err);
+// GET all posts for homepage
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {model: User,
+                attributes: ['username']},
+                {model: Comment,
+                    attributes: ['username']}
+            ],
+
         });
+        res.json(postData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
+// Create Posts using Async method
+router.post("/", async (req, res) => {
+    try {
+        const postData = await Post.create({
+            ...req.body,
+            userId: req.params.user_id
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+// Create Posts without Async method for future references
+// router.post("/", withAuth, (req, res) => {
+//     const body = req.body;
+//     console.log(req.session.userId);
+//     Post.create({ ...body, userId: req.session.userId })
+//         .then(newPost => {
+//             res.json(newPost);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
 
-router.put("/:id", withAuth, (req, res) => {
-    console.log(req.body, req.params.id)
-    Post.update(req.body, {
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(affectedRows => {
-            if (affectedRows > 0) {
-                res.status(200).end();
-            } else {
-                res.status(404).end();
+// Update Posts
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update({
+            title: req.body.title,
+            content: req.body.content
+        },
+            {
+                where: {
+                    id: req.params.id
+                }
+            });
+
+        res.status(200).json(postData);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+// Update Posts without Async method for future references
+// router.put("/:id", withAuth, (req, res) => {
+//     console.log(req.body, req.params.id)
+//     Post.update(req.body, {
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//         .then(affectedRows => {
+//             if (affectedRows > 0) {
+//                 res.status(200).end();
+//             } else {
+//                 res.status(404).end();
+//             }
+//         })
+//         .catch(err => {
+//             res.status(500).json(err);
+//         });
+// });
+// Delte Posts
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id
             }
-        })
-        .catch(err => {
-            res.status(500).json(err);
         });
-});
 
-router.delete("/:id", withAuth, (req, res) => {
-    console.log(req.body, req.params.id)
-    Post.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(affectedRows => {
-            if (affectedRows > 0) {
-                res.status(200).end();
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
+        res.status(200).json(postData);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
+// Update Posts without Async method for future references
+// router.delete("/:id", withAuth, (req, res) => {
+//     console.log(req.body, req.params.id)
+//     Post.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//         .then(affectedRows => {
+//             if (affectedRows > 0) {
+//                 res.status(200).end();
+//             } else {
+//                 res.status(404).end();
+//             }
+//         })
+//         .catch(err => {
+//             res.status(500).json(err);
+//         });
+// });
 
 module.exports = router;
