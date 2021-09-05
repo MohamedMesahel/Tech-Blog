cb85ba4155846f470c034f4bc2621c66023f9e8b
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-05 14:31:38 -0700

    finshing the relationship between the 3 models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
 const User = require('./User');
-const Project = require('./Post');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
+// TODO: Build the relationshipns between the 3 models (user, post, comment)
+User.hasMany(Post, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Post.belongsTo(User, {
+  foreignKey: 'userId'
 });
 
-module.exports = { User, Project };
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
+
+module.exports = { User, Post, Comment };
