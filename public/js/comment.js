8b8f78a48ee956689c37fcb712abadbce1572aa6
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-06 15:05:42 -0700

    commentjs

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const content = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const postId = document.querySelector('textarea[name="post-body"]').value.trim();
+
+    if (content) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                postId,
+                content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
