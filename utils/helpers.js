ff0ff94fc0dbf7e99a52201213d95237dfea01e1
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-09 12:57:14 -0700

    adding format plural hook

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,17 +7,12 @@ module.exports = {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
   },
-  get_emoji: () => {
-    const randomNum = Math.random();
-
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+  // Format plural hook
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
     }
-  },
+
+    return word;
+  }
 };
-// format plural hook
