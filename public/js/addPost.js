3be081fb79d0a3d2c021a107d507b3354e874cb8
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-12 11:59:01 -0700

    test1

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,11 +1,29 @@
 // Create new Post form
-async function newFormHandler(event) {
+const newFormHandler = async (event) => {
     event.preventDefault();
 
     const title = document.querySelector('#titleN').value.trim();
     const content = document.querySelector('#contentN').value.trim();
 
-    const response = await fetch(`/api/posts`, {
+    if (title && content) {
+        const response = await fetch(`/api/posts/`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+    const response = await fetch(`/api/posts/`, {
         method: 'POST',
         body: JSON.stringify({
             title,
